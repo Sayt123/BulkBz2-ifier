9fceb692589547851a2317cc93316abb03de7203
Sayt <sayts.archives@protonmail.com>
2021-05-04 14:31:54 -0500

    Update bzip.bat
    Changed what the first open input said.

--- bzip.bat
+++ bzip.bat
@@ -1,3 +1,3 @@
 @Echo Off
-cd "[YOUR MAPS DIRECTORY HERE]"
+cd "[THE DIRECTORY WHICH YOU WANT TO COMPRESS]"
 FOR %%1 IN (*.*) DO "[C:\Program Files\7-Zip\7z.exe OR YOUR 7zip.exe LOCATION]" a -tbzip2 "%%~nx1.bz2" "%%1"
